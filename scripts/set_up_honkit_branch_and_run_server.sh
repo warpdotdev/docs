7c7765b6b686e55fef3cccf4260fda2cf91e6007
elvis kahoro <elvis@warp.dev>
2022-10-09 03:27:40 -0400

    lint: removing dead scripts since we're no longer using honkit

--- scripts/set_up_honkit_branch_and_run_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# A bash script to set up the honkit branch
-echo "Note: Please commit or stash any changes before running; also make sure that you've rebased on main."
-current_git_branch=$(git symbolic-ref --short HEAD)
-git checkout honkit &&
-git rebase $current_git_branch &&
-npx honkit serve
